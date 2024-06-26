b45de79f41bff1a960cdf8bc189d0af55b4c6d64
alessandro-barate <alebarat@gmail.com>
2024-06-26 16:47:20 +0100

    [UPD] Basic style added

--- index.php
+++ index.php
@@ -1,1 +1,57 @@
 <?php
+
+class Products
+{
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Products</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul class="d-flex justify-between">
+                <li>
+                    <button>Home</button>
+                </li>
+                <li>
+                    <button>Chi Siamo</button>
+                </li>
+                <li>
+                    <button>Punti Vendita</button>
+                </li>
+                <li>
+                    <button>Contattaci</button>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <section>
+                        <h1>ALGAPLANET</h1>
+                    </section>
+
+                    <section>
+
+                    </section>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
